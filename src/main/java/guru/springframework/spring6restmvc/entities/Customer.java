b023b99b56c7565723e8cffa4d04d1b87e482fb5
izharj <eezhar@gmail.com>
2023-04-15 11:30:51 +0800

    created beer and customer entity

---
+++ src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
@@ -0,0 +1,28 @@
+package guru.springframework.spring6restmvc.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    private UUID id;
+    private String name;
+
+    @Version
+    private Integer version;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+
+}
